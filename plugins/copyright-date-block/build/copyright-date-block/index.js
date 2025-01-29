a2ce1bc0c25affb0ad8337ea7e2cb9a57da96bdf
AEMuto <antoine.marseaud@gmail.com>
2025-01-29 11:27:12 +0100

    Refactor Copyright Date Block: update styles, remove unused CSS, and enhance editor functionality

--- plugins/copyright-date-block/build/copyright-date-block/index.js
+++ plugins/copyright-date-block/build/copyright-date-block/index.js
@@ -18,30 +18,23 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _wordpress_block_editor__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_1__);
 /* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @wordpress/components */ "@wordpress/components");
 /* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_wordpress_components__WEBPACK_IMPORTED_MODULE_2__);
-/* harmony import */ var _editor_scss__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./editor.scss */ "./src/copyright-date-block/editor.scss");
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! react/jsx-runtime */ "react/jsx-runtime");
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react/jsx-runtime */ "react/jsx-runtime");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__);
 /**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
-
-
-/**
- * React hook that is used to mark the block wrapper element.
+ * __ (double underscore) is a function that retrieves the translation of text.
+ * useBlockProps: React hook that is used to mark the block wrapper element.
  * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-
-
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * InspectorControls: A slot for the block inspector controls.
+ * PanelBody: A container for a set of controls.
+ * TextControl: A text input field.
+ * './editor.scss': Importing the editor styles. Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
  * Those files can contain any CSS code that gets applied to the editor.
  *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
+
+
 
 
 /**
@@ -53,11 +46,29 @@ __webpack_require__.r(__webpack_exports__);
  * @return {Element} Element to render.
  */
 
-function Edit(props) {
-  const startingYear = props.attributes.startingYear;
-  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("p", {
-    ...(0,_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_1__.useBlockProps)(),
-    children: [(0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_0__.__)('Copyright', 'copyright-date-block'), " \xA9 ", startingYear, " - ", new Date().getFullYear()]
+function Edit({
+  attributes,
+  setAttributes
+}) {
+  const {
+    startingYear
+  } = attributes;
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.Fragment, {
+    children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_1__.InspectorControls, {
+      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(_wordpress_components__WEBPACK_IMPORTED_MODULE_2__.PanelBody, {
+        title: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_0__.__)('Settings', 'copyright-date-block'),
+        children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(_wordpress_components__WEBPACK_IMPORTED_MODULE_2__.TextControl, {
+          label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_0__.__)('Starting Year', 'copyright-date-block'),
+          value: startingYear,
+          onChange: startingYear => setAttributes({
+            startingYear
+          })
+        })
+      })
+    }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("p", {
+      ...(0,_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_1__.useBlockProps)(),
+      children: [(0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_0__.__)('Copyright', 'copyright-date-block'), " \xA9 ", startingYear, " - ", new Date().getFullYear()]
+    })]
   });
 }
 
@@ -159,18 +170,6 @@ function save(props) {
 
 /***/ }),
 
-/***/ "./src/copyright-date-block/editor.scss":
-/*!**********************************************!*\
-  !*** ./src/copyright-date-block/editor.scss ***!
-  \**********************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
-
-
-/***/ }),
-
 /***/ "./src/copyright-date-block/style.scss":
 /*!*********************************************!*\
   !*** ./src/copyright-date-block/style.scss ***!
@@ -239,7 +238,7 @@ module.exports = window["wp"]["i18n"];
   \*********************************************/
 /***/ ((module) => {
 
-module.exports = /*#__PURE__*/JSON.parse('{"$schema":"https://schemas.wp.org/trunk/block.json","apiVersion":3,"name":"copyright-date/copyright-date-block","version":"0.1.0","title":"Copyright Date Block","category":"widgets","icon":"calendar","description":"A block that displays a copyright date.","example":{},"attributes":{"startingYear":{"type":"string","default":2021}},"supports":{"html":false},"textdomain":"copyright-date-block","editorScript":"file:./index.js","editorStyle":"file:./index.css","style":"file:./style-index.css"}');
+module.exports = /*#__PURE__*/JSON.parse('{"$schema":"https://schemas.wp.org/trunk/block.json","apiVersion":3,"name":"copyright-date/copyright-date-block","version":"0.1.0","title":"Copyright Date Block","category":"widgets","icon":"calendar","description":"A block that displays a copyright date.","example":{},"attributes":{"startingYear":{"type":"string","default":2021}},"supports":{"html":false,"align":true,"typography":{"textAlign":true}},"textdomain":"copyright-date-block","editorScript":"file:./index.js","style":"file:./style-index.css"}');
 
 /***/ })
 
--- plugins/copyright-date-block/src/copyright-date-block/edit.js
+++ plugins/copyright-date-block/src/copyright-date-block/edit.js
@@ -1,25 +1,19 @@
 /**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
-import { __ } from '@wordpress/i18n';
-
-/**
- * React hook that is used to mark the block wrapper element.
+ * __ (double underscore) is a function that retrieves the translation of text.
+ * useBlockProps: React hook that is used to mark the block wrapper element.
  * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
-import { PanelBody, TextControl } from '@wordpress/components';
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * InspectorControls: A slot for the block inspector controls.
+ * PanelBody: A container for a set of controls.
+ * TextControl: A text input field.
+ * './editor.scss': Importing the editor styles. Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
  * Those files can contain any CSS code that gets applied to the editor.
  *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
-import './editor.scss';
+import { __ } from '@wordpress/i18n';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import { PanelBody, TextControl } from '@wordpress/components';
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -29,14 +23,25 @@ import './editor.scss';
  *
  * @return {Element} Element to render.
  */
-export default function Edit(props) {
-	const startingYear = props.attributes.startingYear;
+export default function Edit({ attributes, setAttributes }) {
+	const { startingYear } = attributes;
 	return (
-		<p { ...useBlockProps() }>
-			{ __(
-				'Copyright',
-				'copyright-date-block'
-			) } &copy; {startingYear} - { new Date().getFullYear() }
-		</p>
+		<>
+			<InspectorControls>
+				<PanelBody title={__('Settings', 'copyright-date-block')}>
+					<TextControl
+						label={__('Starting Year', 'copyright-date-block')}
+						value={startingYear}
+						onChange={(startingYear) => setAttributes({ startingYear })}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<p {...useBlockProps()}>
+				{__(
+					'Copyright',
+					'copyright-date-block'
+				)} &copy; {startingYear} - {new Date().getFullYear()}
+			</p>
+		</>
 	);
 }
